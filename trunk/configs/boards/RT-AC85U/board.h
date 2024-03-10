6502a9890932a48f7ae0a9ddb811553f37fd4f37
liudexi <451643085@qq.com>
2024-03-11 01:10:05 +0800

    Update board.h

--- trunk/configs/boards/RT-AC85U/board.h
+++ trunk/configs/boards/RT-AC85U/board.h
@@ -2,7 +2,7 @@
 
 #define BOARD_PID	"RT-AC85U"
 #define BOARD_NAME	"RT-AC85U"
-#define BOARD_DESC	"ASUS RT-AC85P Wireless Router"
+#define BOARD_DESC	"ASUS RT-AC85U Wireless Router"
 #define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
 #define BOARD_VENDOR_URL	"https://www.asus.com.cn/"
 #define BOARD_MODEL_URL	"https://www.asus.com.cn/Networking-IoT-Servers/WiFi-Routers/ASUS-WiFi-Routers/RT-AC85P/"
